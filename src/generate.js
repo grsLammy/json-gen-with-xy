bb91f2e15fd5b64bff69260028fae1dbc6cd92a4
grsLammy <grspro98@gmail.com>
2022-06-13 13:23:28 +0530

    update: update metadata structure

--- src/generate.js
+++ src/generate.js
@@ -14,9 +14,15 @@ const generate = async () => {
     console.log("GENERATING METADATA FOR PLOT(0,0) to PLOT(299,299)...")
     for (let i = 0; i < size * size; i++) {
       const rawData = {
-        name: "Gamespad Land",
         description: "plot metadata for Gamespad Land",
-        coordinate: { x: i % size, y: Math.floor(i / size) },
+        name: "Gamespad Land",
+        attributes: [
+          { trait_type: "X COORDINATE", value: (i % size).toString() },
+          {
+            trait_type: "Y COORDINATE",
+            value: Math.floor(i / size).toString(),
+          },
+        ],
       }
       const dataStream = JSON.stringify(rawData, null, 2)
       fs.writeFileSync(`./plot_metadata/${i}.json`, dataStream, "utf-8")
