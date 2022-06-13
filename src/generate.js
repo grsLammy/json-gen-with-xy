3021fa48d15e2451a28dfbc5a152b66a8588b49a
grsLammy <grspro98@gmail.com>
2022-06-13 12:10:07 +0530

    feat: properly handle dir

--- src/generate.js
+++ src/generate.js
@@ -5,14 +5,18 @@ const main = async () => {
   let size = 300
   const dir = "./plot_metadata"
   try {
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir)
+    if (fs.existsSync(dir)) {
+      fs.rmSync(dir, { recursive: true }, (err) => {
+        if (err) throw err
+      })
     }
+    fs.mkdirSync(dir)
     console.log("GENERATING METADATA FOR PLOT(0,0) to PLOT(299,299)...")
     for (let i = 0; i < size * size; i++) {
       const rawData = {
-        x: i % size,
-        y: Math.floor(i / size),
+        name: "Gamespad Land",
+        description: "plot metadata for Gamespad Land",
+        coordinate: { x: i % size, y: Math.floor(i / size) },
       }
       const dataStream = JSON.stringify(rawData, null, 2)
       fs.writeFileSync(`./plot_metadata/${i}.json`, dataStream, "utf-8")
